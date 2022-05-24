4cd0aff34d5274f4a65eb499790daf2812e55e07
Andrii Torkotiuk <torkotiuk@gmail.com>
2022-05-25 01:45:48 +0200

    add color changing in logog & header

---
+++ js/changeColor-in-stickyHeader.js
@@ -0,0 +1,27 @@
+const header = document.querySelector('.header');
+const logo = document.querySelector('.logo');
+const mybutton = document.getElementById('myBtn');
+
+window.onscroll = function () {
+  var top = window.scrollY;
+
+  scrollFunction();
+
+  if (top >= 2200) {
+    header.classList.add('active');
+    logo.classList.add('active');
+  } else {
+    header.classList.remove('active');
+    logo.classList.remove('active');
+  }
+};
+
+// display scrollBtn fn
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+//
--- js/scrollUpBtn.js
+++ js/scrollUpBtn.js
@@ -1,15 +1,13 @@
-//Get the button
-var mybutton = document.getElementById('myBtn');
+// var mybutton = document.getElementById('myBtn');
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = 'block';
-  } else {
-    mybutton.style.display = 'none';
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = 'block';
+//   } else {
+//     mybutton.style.display = 'none';
+//   }
+// }
